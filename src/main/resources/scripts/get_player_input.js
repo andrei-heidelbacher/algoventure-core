957cb88de30d4f6e7f67740bb77e99c1155a0416
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2016-08-09 20:08:12 +0300

    Updated algostorm version.

--- src/main/resources/scripts/get_player_input.js
+++ src/main/resources/scripts/get_player_input.js
@@ -1,5 +1,5 @@
 function getPlayerInput(objectManager, actorId) {
-    var player = objectManager.get(actorId)
+    var player = objectManager.get(actorId);
     var lastInput = player.get("lastInput");
     player.remove("lastInput");
     return lastInput;
